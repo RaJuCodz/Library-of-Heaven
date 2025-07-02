8ce96ca9cd6d5011efee3fa3473d19b670aa44e9
RajuCodz <itsraajjjuuuu@gmail.com>
2025-07-02 11:34:16 +0530

    fixing deployement issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./", // ensure relative asset paths in index.html
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // match your Vercel distDir
+    emptyOutDir: true, // clear old files on each build
+  },
+});
